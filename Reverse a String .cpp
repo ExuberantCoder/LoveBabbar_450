15218f2c3bcf67ccaf71c566703b4bffefb568b9
ExuberantCoder <64614610+ExuberantCoder@users.noreply.github.com>
2021-06-09 12:01:48 +0530

    Update Reverse a String .cpp

--- Reverse a String .cpp
+++ Reverse a String .cpp
@@ -1,12 +1,13 @@
 //User function Template for C++
 
 string reverseWord(string str){
-    
-  //Your code here
-  
-  for(int i=0;i<str.length()/2;i++)
+    char a;
+    int b=str.length();
+  for (int i=0;i<b/2;i++)
   {
-     swap(str[i],str[str.length()-1-i]);
+      a=str[i];
+      str[i]=str[b-1-i];
+      str[b-1-i]=a;
   }
   return str;
 }
